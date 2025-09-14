beeb750e81f2c275d19d0b89da72f896f5f33d19
Adrian-Pierre <Adrian-Pierre@users.noreply.github.com>
2025-09-14 13:10:31 +0800

    第一次anpier.cn修改

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://adrian-pierre.github.io",
+	site: "https://anpier.cn",
 	base: "",
 	trailingSlash: "always",
 	integrations: [
